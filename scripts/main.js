ff2a47dbb9885877e985bafc1dcd1a34a794080b
Popovich Aleksandr <mr.alex2506@inbox.ru>
2017-05-09 13:43:44 +0300

    “message”

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+;(function($){
+	'use strict';
+	 
+		$('.ba-slider').slick({
+			dots: true,
+			slide: '.ba-slide',
+			prevArrow: '.ba-prev',
+			nextArrow: '.ba-next',
+		});
+		
+
+	})(jQuery);
